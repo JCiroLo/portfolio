b81d4eed89d332e71846a41fd46f15f72b5b0a81
JCiroLo <80493825+JCiroLo@users.noreply.github.com>
2024-02-23 18:04:44 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  publicPath: '/',
   transpileDependencies: true,
   lintOnSave: false,
   css: {
